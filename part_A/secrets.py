ec4213f6b3ee6c1399bda1b2eae8e2e9fd760033
mcginnisa <mcginnisalexander@gmail.com>
2019-11-30 17:20:23 -0800

    Update secrets.py

--- part_A/secrets.py
+++ part_A/secrets.py
@@ -1,5 +1,5 @@
 sender_address = "professionalemail@firemail.cc"
-sender_password = "[[141518]]"
+sender_password = ""
 sender_server = 'mail.cock.li'
 sender_port = 587
 recipient_address = "mcginnisalexander@gmail.com"
